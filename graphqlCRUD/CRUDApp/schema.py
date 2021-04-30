64e2b483bf5222016d0b473a1d06ab7af2fde35f
Ashutosh <pythondev@ongraph.com>
2021-04-30 13:28:57 +0530

    Add Custome Model

--- graphqlCRUD/CRUDApp/schema.py
+++ graphqlCRUD/CRUDApp/schema.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from .models import Category,Quizzes,Question,Answer
 from django.contrib.auth.hashers import make_password
 from graphene_django import DjangoObjectType
 import graphene
@@ -8,26 +9,33 @@ import graphene
 class UserType(DjangoObjectType):
     class Meta:
         model = User
-        # fields=('id','username','password','email')
-class Query(graphene.ObjectType):
-    #Pass id field as query parameter
-    users = graphene.List(UserType,id=graphene.Int())
-    # @graphene.resolve_only_args
-    def resolve_users(self,info,**args):
-        if args.get('id') :
-            return User.objects.filter(pk=args.get('id'))
-        else:
-            return User.objects.all()
-#Mutation
+        fields=('id','username','password','email')
 
+class CategoryType(DjangoObjectType):
+    class Meta:
+        model = Category
+        fields = ("id","name")
+class QuizzesType(DjangoObjectType):
+    class Meta:
+        model = Quizzes
+        fields = ("id","title","category")
+
+class QuestionType(DjangoObjectType):
+    class Meta:
+        model = Question
+        fields = ("title","quiz")
+
+class AnswerType(DjangoObjectType):
+    class Meta:
+        model = Answer
+        fields = ("question","answer_text")
+#Mutation
 class CreateUser(graphene.Mutation):
     class Input:
         username = graphene.String()
         password = graphene.String()
         email = graphene.String()
     ok = graphene.Boolean()
-    # us = graphene.Field(lambda : UserType)
-
     def mutate(self,info,**args):
         created_user = User.objects.create(username=args.get('username'))
         created_user.password = make_password(args.get('password'))
@@ -71,5 +79,42 @@ class Mutation(graphene.ObjectType):
     create_user = CreateUser.Field()
     delete_user = DeleteUser.Field()
     update_user = UpdateUser.Field()
+
+class Query(graphene.ObjectType):
+    all_quizzes = graphene.List(QuizzesType,id=graphene.Int())
+    all_question = graphene.List(QuestionType,id=graphene.Int())
+    all_category = graphene.List(CategoryType,id=graphene.Int())
+    all_answer = graphene.List(AnswerType,id=graphene.Int())
+    users = graphene.List(UserType, id=graphene.Int())
+
+    def resolve_users(self, info, **args):
+        if args.get('id'):
+            return User.objects.filter(pk=args.get('id'))
+        else:
+            return User.objects.all()
+
+    def resolve_all_quizzes(self,info,**args):
+        if args.get('id'):
+            return Quizzes.objects.filter(id=args.get('id'))
+        else:
+            return Quizzes.objects.all()
+
+    def resolve_all_question(self,info,**args):
+        if args.get('title'):
+            return Question.objects.get(title=args.get('title'))
+        else:
+            return Question.objects.all()
+
+    def resolve_all_category(self,info,**args):
+        if args.get('id'):
+            return Category.objects.get(pk=args.get('id'))
+        else:
+            return Category.objects.all()
+
+    def resolve_all_answer(self,info,**args):
+        if args.get('question'):
+            return Answer.objects.get(question=args.get('question'))
+        else:
+            return Answer.objects.all()
 
 schema = graphene.Schema(query=Query ,mutation=Mutation)
